084a99df937e69b11897950170b286626d99f6a0
Volodymyr-Horban <90247333+Volodymyr-Horban@users.noreply.github.com>
2024-12-05 23:41:17 +0200

    TODO3

--- lesoon-1/module-1/tasks2.js
+++ lesoon-1/module-1/tasks2.js
@@ -12,16 +12,42 @@
 // У змінній min лежить число від 0 до 59. Визначте у котру чверть години
 // Попадає це число (у першу, другу, третю або четверту).
 
-const min = Number(prompt('Enter your number'));
+// const min = Number(prompt('Enter your number'));
 
-if (min >0 && min <= 15) {
-    alert('First quarter')
-} else if (min >16 && min <= 30) {
-    alert('Second quarter')
-} else if (min >31 && min <= 45) {
-    alert('Third quarter')
-} else if (min > 45 && min < 60 || min === 0) {
-    alert('Fourth quarter')
-} else {
-    alert('Enter a different number')
+// if (min >0 && min <= 15) {
+//     alert('First quarter')
+// } else if (min >16 && min <= 30) {
+//     alert('Second quarter')
+// } else if (min >31 && min <= 45) {
+//     alert('Third quarter')
+// } else if (min > 45 && min < 60 || min === 0) {
+//     alert('Fourth quarter')
+// } else {
+//     alert('Enter a different number')
+// }
+
+
+
+//TODO: 3 ===================================
+// Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+
+const num = +prompt('Enter 1 or 2 or 3 or 4');
+
+switch(num) {
+    case 1:
+        alert('Winter');
+        break;
+    case 2:
+        alert('Spring');
+        break;
+    case 3:
+        alert ('Summer');
+        break;
+    case 4 :
+        alert('Autumn');
+        break;
+    default:
+        alert('Enter a different number');
 }
